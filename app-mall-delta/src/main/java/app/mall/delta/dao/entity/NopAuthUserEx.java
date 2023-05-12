793515f63f7f1948422968a89050784c7af0f471
canonical <canonical_entropy@163.com>
2023-05-12 09:54:38 +0800

    提交delta示例代码

---
+++ app-mall-delta/src/main/java/app/mall/delta/dao/entity/NopAuthUserEx.java
@@ -0,0 +1,13 @@
+package app.mall.delta.dao.entity;
+
+import io.nop.api.core.annotations.biz.BizObjName;
+import app.mall.delta.dao.entity._gen._NopAuthUserEx;
+
+
+@BizObjName("NopAuthUser")
+public class NopAuthUserEx extends _NopAuthUserEx{
+    public NopAuthUserEx(){
+    }
+
+
+}
